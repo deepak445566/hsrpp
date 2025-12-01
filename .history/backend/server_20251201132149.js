2dee2c644085042d756547b755dff9dde3565e63
deepak445566 <deepakpandey1212d@gmail.com>
2025-12-01 13:22:29 +0530

    klmn

--- .history/backend/server_20251201132149.js
+++ .history/backend/server_20251201132149.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://bookingshsrp.com','https://bookingmynumberplatehsrp.com', 'http://localhost:5173'],
+  origin: ['https://bookingshsrp.com','' 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
